987591aec7771cb6af168a7526b76fd505ad3a19
Erin K <erin@penguinstampede.com>
2017-03-13 15:32:22 -0700

    sliders on the resume

--- _assets/javascripts/resume.js
+++ _assets/javascripts/resume.js
@@ -1,3 +1,32 @@
+//= require jquery/dist/jquery
+//= require foundation-sites/js/foundation.core
+//= require foundation-sites/js/foundation.util.keyboard
+//= require foundation-sites/js/foundation.util.triggers
+//= require foundation-sites/js/foundation.util.motion
+//= require foundation-sites/js/foundation.util.touch
+//= require foundation-sites/js/foundation.util.mediaQuery
+//= require foundation-sites/js/foundation.slider
+
+$(function(){
+  $(document).foundation();
+  $('.sidebar .skills ul li').each(function(key, el){
+    var the_slider = $('<div class="slider" data-slider></div>');
+    the_slider.append('<span class="slider-handle" data-slider-handle></span>');
+    the_slider.append('<span class="slider-fill" data-slider-fill></span>');
+    the_slider.append('<input type="hidden">');
+    var the_span = $(el).find('span');
+    var rating = the_span.html();
+    var slider_options = {
+      start: 1,
+      end: 5,
+      initialStart: rating,
+      draggable: false
+    };
+    the_span.replaceWith(the_slider);
+    var slider = new Foundation.Slider(the_slider, slider_options);
+  });
+});
+
 var el = document.body, className = 'phantom';
 if(window._phantom){
   if(el.classList){
--- _includes/skills.md
+++ _includes/skills.md
@@ -1,13 +1,10 @@
 ### Skills
 
 - Web Applications <span>5</span>
-- Mobile-First Design <span>5</span>
 - User Experience Design <span>4</span>
 - Website Analytics <span>4</span>
 - A/B Testing <span>4</span>
 - Email Creation <span>5</span>
-- CMS Administration <span>4</span>
 - Wordpress Plugins + Themes <span>4</span>
 - API Integration <span>4</span>
 - Build Automation + Testing <span>3</span>
-- Version Control <span>4</span>
