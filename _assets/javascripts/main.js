7632535225be5f9ac3403e0c78f5660b5926ea2d
Erin K <erin@penguinstampede.com>
2017-03-13 19:17:12 -0700

    adding validation to the form

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -10,10 +10,55 @@
 $(function(){
   $(document).foundation();
 
-  $('#tozap .submit').on('click', function(){
-    var formdata = $('#tozap').serialize();
-    $.post( "https://hooks.zapier.com/hooks/catch/2056452/mcmxnb/", formdata, function( data ) {
-      console.log( data );
+  $('#tozap .submit').on('click', function(ev){
+    var err = false;
+    ev.preventDefault();
+    $(this).addClass('disabled');
+    $('#contact .callout').hide();
+    $('#contact .callout p').html('');
+    var emailcheck = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if ( (emailcheck.test($('#youremail').val()) == false) || ($('#youremail').val() == '') ) {
+      err = true;
+      errmsg = 'Please make sure your email address is correct before submitting.';
+    }
+    $('input:required').each(function(key, val){
+      if ($(this).val() == ''){
+        err = true;
+        errmsg = 'Please make sure all required fields are filled out before submitting.';
+      }
     });
+    $('select:required').each(function(key, val){
+      if ($(this).val() == ''){
+        err = true;
+        errmsg = 'Please make sure all required fields are filled out before submitting.';
+      }
+    });
+    $('textarea:required').each(function(key, val){
+      if ($(this).val() == ''){
+        err = true;
+        errmsg = 'Please make sure all required fields are filled out before submitting.';
+      }
+    });
+    if (!err) {
+      var formdata = $('#tozap').serialize();
+      var tozap = $.post('https://hooks.zapier.com/hooks/catch/2056452/mcmxnb/', formdata);
+      tozap.done(function(data){
+        if (data.status == 'success'){
+          ga('send', 'pageview', '/contact');
+          $('.complete').fadeOut();
+          $('#tozap').fadeOut(400, function(){
+            $('#thanks').fadeIn();
+          });
+        } else {
+          console.log(data);
+          $('#contact .callout p').html(data.error);
+          $('#contact .callout').fadeIn();
+        }
+      });
+    } else{
+      $(this).removeClass('disabled');
+      $('#contact .callout p').html(errmsg);
+      $('#contact .callout').fadeIn();
+    }
   });
 });
